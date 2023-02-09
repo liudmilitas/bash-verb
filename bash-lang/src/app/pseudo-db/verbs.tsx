1e3350e4a4dd2605e230dbf161361af122349837
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2023-02-09 09:18:08 +0100

    add present simple verb function

--- bash-lang/src/app/pseudo-db/verbs.tsx
+++ bash-lang/src/app/pseudo-db/verbs.tsx
@@ -1,3 +1,51 @@
-const verbs: never[] = [] 
+// ГЛАГОЛ - ҠЫЛЫМ
+
+const verbs: object[] = [
+  {
+    id: 1,
+    imperative: "аша",
+    properties: {
+      is_regular: true,
+      is_soft: false,
+      is_last_vowel: true,
+    },
+    translations: {
+      ru: "есть",
+      eng: "to eat",
+    },
+  },
+  {
+    id: 2,
+    imperative: "эшлә",
+    properties: {
+      is_regular: true,
+      is_soft: true,
+      is_last_vowel: true,
+    },
+    translations: {
+      ru: "работать",
+      eng: "to work",
+    },
+  },
+];
+
+function generatePresentSimple (verb: any) {
+    if (verb.properties.is_regular) {
+        if (verb.properties.is_last_vowel) {
+            if (verb.properties.is_soft) {
+                return {
+                    first_singular: verb.imperative + "йем",
+                }
+            } else {
+
+            }
+
+        } else {
+
+        }
+    } else {
+        return 'Неправильный глагол!'
+    }
+};
 
 export default verbs;
