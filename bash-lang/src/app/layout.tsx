ce10ac0896546bbff926640bd57a1be16e4c45c9
Liudmila Kazakbaeva <83576552+liudmilitas@users.noreply.github.com>
2023-02-11 19:17:09 +0100

    restructured code, fixed styling

--- bash-lang/src/app/layout.tsx
+++ bash-lang/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Montserrat } from "@next/font/google";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const montserrat = Montserrat({
   subsets: ["cyrillic", "latin", "cyrillic-ext"],
@@ -22,7 +24,11 @@ export default function RootLayout({
           "bg-gradient-to-r from-emerald-500 to-lime-600 flex flex-col justify-around items-center lg:px-16 md:px-12 sm:px-6 p-4 h-full min-h-screen pattern"
         }
       >
+        <Header />
+        <main className="bg-white h-full rounded shadow-2xl w-full min-h-[500px] max-w-[800px] flex flex-col content-center items-center justify-center p-2 lg:p-4">
         {children}
+        </main> 
+        <Footer />
       </body>
     </html>
   );
